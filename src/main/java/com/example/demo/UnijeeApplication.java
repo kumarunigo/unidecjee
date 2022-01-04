6ebcd4db5eeb9381e265757349634bfeff3a6946
java <96713587+kumarunigo@users.noreply.github.com>
2022-01-04 14:18:59 -0600

    jee setup

---
+++ src/main/java/com/example/demo/UnijeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnijeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnijeeApplication.class, args);
+	}
+
+}
